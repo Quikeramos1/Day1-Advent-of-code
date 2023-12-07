983764d072e39689c0cda2f07449442253c6dd6d
Quike Ramos Fuster <quike@MacBook-Pro-de-Quike.local>
2023-12-07 21:47:30 +0100

    ajustes menorres, aun no da resutaldo correcto

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-dnums = {"one":1, "two":2, "three":3, "four":4,"five":5,
-             "six":6,"seven":7,"eight":8,"nine":9}
+dnums = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
+         "six": 6, "seven": 7, "eight": 8, "nine": 9}
 
 file = input("¿Qué archivo deseas abrir? ")
 
 try:
-    open_file = open(file)
-    
-except:
+    open_file = open(file, "r")
+
+except FileNotFoundError:
     print("El archivo indicado no existe\no no se ha podido abrir")
     exit()
 
-lista_linea = [] 
-lista_text = [] 
-lista_final = []  
+lista_final = []
 contador = 0
 
 for linea in open_file:
     contador += 1
     linea = linea.rstrip()
+    lista_linea = []
+    lista_text = []
+
     for i in linea:
-                
         lista_text.append(i)
 
         try:
@@ -27,37 +27,20 @@ for linea in open_file:
             lista_linea.append(i)
 
         except:
-            text_final = "".join(lista_text)
-            text_final.lower()
+            text_final = "".join(lista_text).lower()
             for num in dnums:
                 if num in text_final:
                     num = dnums[num]
                     lista_linea.append(num)
                     lista_text = []
-                
-            
-         
+                    break
 
-    digitos = str(lista_linea[0])+str(lista_linea[-1])
-
-        
-        
-            
-              
-                       
-    
-    
+    digitos = str(lista_linea[0]) + str(lista_linea[-1])
     digitos = int(digitos)
     lista_final.append(digitos)
-    lista_linea = []
-    lista_text = []
-    resultado = sum(lista_final)
 
+resultado = sum(lista_final)
 
-    
 print(f"El documento contiene", contador, "lineas")
-print(f"La cantidad de valores recopilados es de",len(lista_final))
-print(f"La suma de todos los valores recopilados es",resultado)
-
-
-
+print(f"La cantidad de valores recopilados es de", len(lista_final))
+print(f"La suma de todos los valores recopilados es", resultado)
