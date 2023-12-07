b3d1c4bdd18ccab6d2935746b7ac22e786c12fb1
Quike Ramos Fuster <quike@MacBook-Pro-de-Quike.local>
2023-12-07 21:07:26 +0100

    adaptado código para segunda parte de dia 1

--- main.py
+++ main.py
@@ -1,39 +1,56 @@
-'''The newly-improved calibration document consists of lines of text;
-each line originally contained a specific calibration value that the
-Elves now need to recover. On each line, the calibration value can be 
-found by combining the first digit and the last digit (in that order) 
-to form a single two-digit number.'''
+dnums = {"one":1, "two":2, "three":3, "four":4,"five":5,
+             "six":6,"seven":7,"eight":8,"nine":9}
 
 file = input("¿Qué archivo deseas abrir? ")
 
 try:
     open_file = open(file)
+    
 except:
     print("El archivo indicado no existe\no no se ha podido abrir")
     exit()
 
-lista_linea = []  
+lista_linea = [] 
+lista_text = [] 
 lista_final = []  
 contador = 0
+
 for linea in open_file:
     contador += 1
     linea = linea.rstrip()
     for i in linea:
-        
-        try: 
-            int(i)
+                
+        lista_text.append(i)
+
+        try:
+            i = int(i)
             lista_linea.append(i)
-            digitos = 0    
-            digitos = lista_linea[0]+lista_linea[-1]
-            
-            
-            
+
         except:
-            continue 
+            text_final = "".join(lista_text)
+            text_final.lower()
+            for num in dnums:
+                if num in text_final:
+                    num = dnums[num]
+                    lista_linea.append(num)
+                    lista_text = []
+                
+            
+         
+
+    digitos = str(lista_linea[0])+str(lista_linea[-1])
+
+        
+        
+            
+              
+                       
+    
     
     digitos = int(digitos)
     lista_final.append(digitos)
     lista_linea = []
+    lista_text = []
     resultado = sum(lista_final)
 
 
@@ -41,5 +58,6 @@ for linea in open_file:
 print(f"El documento contiene", contador, "lineas")
 print(f"La cantidad de valores recopilados es de",len(lista_final))
 print(f"La suma de todos los valores recopilados es",resultado)
+
 
 
--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
-Resolución pruebas de Advent of Code
+Resolución pruebas de Advent of Code 2023
+
+--- Primera Parte ---
+
+The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+
+El primer resultado del dia 1 es 55108
+
+--- Segunda Parte ---
+
+Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
